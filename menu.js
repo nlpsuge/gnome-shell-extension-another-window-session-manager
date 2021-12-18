6a9d4bc0055a56d0818d194ca9c20bb260496132
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-12-19 00:10:54 +0800

    Add menu.js

---
+++ menu.js
@@ -0,0 +1,15 @@
+
+const { GObject } = imports.gi;
+
+const PanelMenu = imports.ui.panelMenu;
+
+var AwsIndicator = GObject.registerClass(
+class AwsIndicator extends PanelMenu.Button {
+
+    _init() {
+
+    }
+
+    
+
+});
