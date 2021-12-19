78cde660c2eb329387aac8ed3dc73edce47f6995
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-12-20 00:41:27 +0800

    Look up the whitelist while closing windows

--- closeSession.js
+++ closeSession.js
@@ -4,6 +4,7 @@ var CloseSession = class {
     constructor() {
         this._skip_app_with_multiple_windows = true;
         this._defaultAppSystem = Shell.AppSystem.get_default();
+        this.whitelist = ['org.gnome.Terminal.desktop'];
     }
 
     closeWindows() {
@@ -11,13 +12,25 @@ var CloseSession = class {
         let running_apps = this._defaultAppSystem.get_running();
         for (const app of running_apps) {
             const app_name = app.get_name();
-            if (this._skip_app_with_multiple_windows && app.get_n_windows() > 1) {
+            if (this._skip_multiple_windows(app)) {
                 log(`Skipping ${app.get_name()} because it has more than one windows`);
                 continue;
             }
             log(`Closing ${app_name}`);
             app.request_quit();
         }
+    }
+
+    _skip_multiple_windows(shellApp) {
+        if (shellApp.get_n_windows() > 1 && this._skip_app_with_multiple_windows) {
+            const app_id = shellApp.get_id();
+            if (this.whitelist.includes(app_id)) {
+                log(`${shellApp.get_name()} / ${app_id} in the whitelist.`);
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 
     destroy() {
