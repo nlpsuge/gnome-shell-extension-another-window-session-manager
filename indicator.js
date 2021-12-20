99f08262ade790c7f0997c3a240d73694f79bcd8
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-12-20 15:20:43 +0800

    Use customized symbolic icon

--- indicator.js
+++ indicator.js
@@ -1,6 +1,9 @@
 
 const { GObject, St, Gio } = imports.gi;
 
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
 const PanelMenu = imports.ui.panelMenu;
 
 var AwsIndicator = GObject.registerClass(
@@ -10,8 +13,9 @@ class AwsIndicator extends PanelMenu.Button {
         super._init(0.0, "Another Window Session Manager");
         
         // Add an icon
+        let iconPath = `${Me.path}/icons/restore-symbolic.svg`;
         let icon = new St.Icon({
-            gicon: new Gio.ThemedIcon({name: 'face-laugh-symbolic'}),
+            gicon: Gio.icon_new_for_string(`${iconPath}`),
             style_class: 'system-status-icon'
         });
         this.add_child(icon);
