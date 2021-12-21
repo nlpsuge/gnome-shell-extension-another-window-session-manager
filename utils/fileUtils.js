5d949cb14dfd012cf516548dcd9997dd467d8cd7
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-12-21 15:07:34 +0800

    Fix: Some code accessed the property 'sessions_path' on the module 'fileUtils'. That property was defined with 'let' or 'const' inside the module. This was previously supported, but is not correct according to the ES6 standard. Any symbols to be exported from a module must be defined with 'var'. The property access will work as previously for the time being, but please fix your code anyway.

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -1,9 +1,9 @@
 const { Gio, GLib } = imports.gi
 
-const default_sessionName = 'defaultSession';
+var default_sessionName = 'defaultSession';
 const home_dir = GLib.get_home_dir();
 const config_path_base = GLib.build_filenamev([home_dir, '.config', 'xsession-manager']);
-const sessions_path = GLib.build_filenamev([config_path_base, 'sessions']);
+var sessions_path = GLib.build_filenamev([config_path_base, 'sessions']);
 
 function get_sessions_path() {
     return sessions_path;
