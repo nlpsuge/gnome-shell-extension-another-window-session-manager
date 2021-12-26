c3c5f945f334e1d5b2b47a54acc6afb6eae82bfc
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-12-26 15:32:45 +0800

    Use a different folder name to save sessions. But this extension can restore `xsm`'s session file, but cannot move them for now due to desktop file id  missing

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -4,7 +4,7 @@ const { Gio, GLib } = imports.gi
 
 var default_sessionName = 'defaultSession';
 const home_dir = GLib.get_home_dir();
-const config_path_base = GLib.build_filenamev([home_dir, '.config', 'xsession-manager']);
+const config_path_base = GLib.build_filenamev([home_dir, '.config', 'another-window-session-manager']);
 var sessions_path = GLib.build_filenamev([config_path_base, 'sessions']);
 
 function get_sessions_path() {
