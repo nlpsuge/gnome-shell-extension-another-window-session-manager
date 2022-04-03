e24f2aca011a3825e220eb6a94d360a6f117bf23
m^n <2271720+nlpsuge@users.noreply.github.com>
2022-04-03 18:00:40 +0800

    Fix: parse string to float to compare versions

--- utils/gnomeVersion.js
+++ utils/gnomeVersion.js
@@ -1,10 +1,13 @@
 const Config = imports.misc.config; 
 
-const GNOME_VERSION = Config.PACKAGE_VERSION;
+// '41.beta' => 41
+// '41.4' => 41.4
+// '3.38.beta' => 3.38
+const GNOME_VERSION = parseFloat(Config.PACKAGE_VERSION);
 
 
 function isOlderThan42() {
-    return GNOME_VERSION < '42';
+    return GNOME_VERSION < 42;
 }
 
 
