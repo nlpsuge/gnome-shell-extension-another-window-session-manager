3a4eb312587a138936200e793eed2440b9cc5d3f
m^n <2271720+nlpsuge@users.noreply.github.com>
2022-07-18 00:50:30 +0800

    Support window edge tiling

--- windowTilingSupport.js
+++ windowTilingSupport.js
@@ -6,12 +6,16 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
 const Log = Me.imports.utils.log;
+const PrefsUtils = Me.imports.utils.prefsUtils;
 
-var windowsAboutToTileMap = new Map();
+
+var windowsAboutToTileMap = null;
 
 var WindowTilingSupport = class {
 
     constructor() {
+        windowsAboutToTileMap = new Map();
+
         this.resizing = new Resizing();
     }
 
@@ -25,6 +29,9 @@ class Resizing {
 
     constructor() {
         this._log = new Log.Log();
+        this._prefsUtils = new PrefsUtils.PrefsUtils();
+        this._settings = this._prefsUtils.getSettings();
+
         this._defaultAppSystem = Shell.AppSystem.get_default();
 
         this._grabOpBeginId = global.display.connect('grab-op-begin', this._grabOpBegin.bind(this));
@@ -32,52 +39,60 @@ class Resizing {
     }
 
     _grabOpBegin(display, grabbedWindow, grabOp) {
-        log(windowsAboutToTileMap.size)
+        if (!windowsAboutToTileMap) return;
+        if (!(Meta.GrabOp.RESIZING_E === grabOp || Meta.GrabOp.RESIZING_W === grabOp)) {
+            windowAboutToResize.delete(grabbedWindow);
+            return;
+        }
         const window_tiling = windowsAboutToTileMap.get(grabbedWindow);
         if (!window_tiling) return;
+        
+        if (!this._settings.get_boolean('restore-window-tiling')) return;
 
         const window_tile_for = window_tiling.window_tile_for;
         const shellApp = this._defaultAppSystem.lookup_app(window_tile_for.desktop_file_id);
         if (!shellApp) return;
         const windows = shellApp.get_windows();
         if (!windows || !windows.length) return;
-        log('_restore Tiling ' + grabbedWindow.get_title())
         let windowAboutToResize;
         if (windows.length === 1) {
             windowAboutToResize = windows[0];
         } else {
-            windowAboutToResize = windows[0];
-        }
-
-        log('windowAboutToResize ' + windowAboutToResize.get_title())
-
-        if (Meta.GrabOp.RESIZING_E === grabOp || Meta.GrabOp.RESIZING_W === grabOp) {
-            log('grab Op Begin ' + grabbedWindow.get_title())
-            this._sizeChangedId = grabbedWindow.connect('size-changed', () => {
-                const grabbedWindowRect = grabbedWindow.get_frame_rect();
-                const windowAboutToResizeRect = windowAboutToResize.get_frame_rect();
-                const grabbedWindowOnLeftSide = grabbedWindowRect.x < windowAboutToResizeRect.x;
-                log('_size Changed ' + grabbedWindow.get_title() + ' ' + grabOp + ' ' + grabbedWindowOnLeftSide)
-                let xywh = null;
-                if (grabbedWindowOnLeftSide) {
-                    xywh = [grabbedWindowRect.width,
-                            windowAboutToResizeRect.y,
-                            windowAboutToResizeRect.width - (grabbedWindowRect.width - windowAboutToResizeRect.x),
-                            windowAboutToResizeRect.height];
-                } else
-                    xywh = [windowAboutToResizeRect.x,
-                            windowAboutToResizeRect.y,
-                            grabbedWindowRect.x,
-                            windowAboutToResizeRect.height];
-                if (xywh) {
-                    windowAboutToResize.move_resize_frame(false, ...xywh);
+            // Match title
+            for (const win of windows) {
+                if (win.get_title() === window_tile_for.window_title) {
+                    windowAboutToResize = win;
+                    break;
                 }
-
-            });
+            }
         }
+
+        this._sizeChangedId = grabbedWindow.connect('size-changed', () => {
+            const grabbedWindowRect = grabbedWindow.get_frame_rect();
+            const windowAboutToResizeRect = windowAboutToResize.get_frame_rect();
+            const grabbedWindowOnLeftSide = grabbedWindowRect.x < windowAboutToResizeRect.x;
+            let xywh = null;
+            if (grabbedWindowOnLeftSide) {
+                xywh = [grabbedWindowRect.width,
+                        windowAboutToResizeRect.y,
+                        windowAboutToResizeRect.width - (grabbedWindowRect.width - windowAboutToResizeRect.x),
+                        windowAboutToResizeRect.height];
+            } else
+                xywh = [windowAboutToResizeRect.x,
+                        windowAboutToResizeRect.y,
+                        grabbedWindowRect.x,
+                        windowAboutToResizeRect.height];
+            if (xywh) {
+                windowAboutToResize.move_resize_frame(false, ...xywh);
+            }
+
+        });
     }
 
     _grabOpEnd(display, grabbedWindow, grabOp) {
+        if (!(Meta.GrabOp.RESIZING_E === grabOp || Meta.GrabOp.RESIZING_W === grabOp)) return;
+        if (!windowsAboutToTileMap) return;
+        if (!this._settings.get_boolean('restore-window-tiling')) return;
         const window_tiling = windowsAboutToTileMap.get(grabbedWindow);
         if (!window_tiling) return;
 
@@ -85,7 +100,6 @@ class Resizing {
             grabbedWindow.disconnect(this._sizeChangedId);
             this._sizeChangedId = 0;
         }
-
 
     }
 
