55955b03fd29c95583ed6a4c8d275cd5b0c6e2b4
m^n <2271720+nlpsuge@users.noreply.github.com>
2022-07-19 01:43:41 +0800

    Untile if moving one of tiled windows to cause x, y, width, height to be changed

--- windowTilingSupport.js
+++ windowTilingSupport.js
@@ -34,38 +34,28 @@ class Resizing {
 
         this._defaultAppSystem = Shell.AppSystem.get_default();
 
+        // Store Meta.Window currently grabbed that can be untiled
+        this._grabbedWindowsAboutToUntileMap = new Map();
+
         this._grabOpBeginId = global.display.connect('grab-op-begin', this._grabOpBegin.bind(this));
         this._grabOpEndId = global.display.connect('grab-op-end', this._grabOpEnd.bind(this));
     }
 
     _grabOpBegin(display, grabbedWindow, grabOp) {
         if (!windowsAboutToTileMap) return;
-        if (!(Meta.GrabOp.RESIZING_E === grabOp || Meta.GrabOp.RESIZING_W === grabOp)) {
-            windowAboutToResize.delete(grabbedWindow);
-            return;
-        }
         const window_tiling = windowsAboutToTileMap.get(grabbedWindow);
         if (!window_tiling) return;
+
+        if (grabOp === Meta.GrabOp.MOVING) {
+            const oldGrabbedWindowRect = grabbedWindow.get_frame_rect().copy();
+            this._grabbedWindowsAboutToUntileMap.set(grabbedWindow, oldGrabbedWindowRect);
+            return;
+        }
         
         if (!this._settings.get_boolean('restore-window-tiling')) return;
 
-        const window_tile_for = window_tiling.window_tile_for;
-        const shellApp = this._defaultAppSystem.lookup_app(window_tile_for.desktop_file_id);
-        if (!shellApp) return;
-        const windows = shellApp.get_windows();
-        if (!windows || !windows.length) return;
-        let windowAboutToResize;
-        if (windows.length === 1) {
-            windowAboutToResize = windows[0];
-        } else {
-            // Match title
-            for (const win of windows) {
-                if (win.get_title() === window_tile_for.window_title) {
-                    windowAboutToResize = win;
-                    break;
-                }
-            }
-        }
+        const windowAboutToResize = this._getWindowAboutToResize(window_tiling);
+        if (!windowAboutToResize) return;
 
         this._sizeChangedId = grabbedWindow.connect('size-changed', () => {
             const grabbedWindowRect = grabbedWindow.get_frame_rect();
@@ -73,15 +63,19 @@ class Resizing {
             const grabbedWindowOnLeftSide = grabbedWindowRect.x < windowAboutToResizeRect.x;
             let xywh = null;
             if (grabbedWindowOnLeftSide) {
-                xywh = [grabbedWindowRect.width,
-                        windowAboutToResizeRect.y,
-                        windowAboutToResizeRect.width - (grabbedWindowRect.width - windowAboutToResizeRect.x),
-                        windowAboutToResizeRect.height];
-            } else
-                xywh = [windowAboutToResizeRect.x,
-                        windowAboutToResizeRect.y,
-                        grabbedWindowRect.x,
-                        windowAboutToResizeRect.height];
+                xywh = [
+                    grabbedWindowRect.width,
+                    windowAboutToResizeRect.y,
+                    windowAboutToResizeRect.width - (grabbedWindowRect.width - windowAboutToResizeRect.x),
+                    windowAboutToResizeRect.height];
+            } else {
+                xywh = [
+                    windowAboutToResizeRect.x,
+                    windowAboutToResizeRect.y,
+                    grabbedWindowRect.x,
+                    windowAboutToResizeRect.height];
+            }
+            
             if (xywh) {
                 windowAboutToResize.move_resize_frame(false, ...xywh);
             }
@@ -90,11 +84,28 @@ class Resizing {
     }
 
     _grabOpEnd(display, grabbedWindow, grabOp) {
-        if (!(Meta.GrabOp.RESIZING_E === grabOp || Meta.GrabOp.RESIZING_W === grabOp)) return;
-        if (!windowsAboutToTileMap) return;
-        if (!this._settings.get_boolean('restore-window-tiling')) return;
-        const window_tiling = windowsAboutToTileMap.get(grabbedWindow);
-        if (!window_tiling) return;
+        const oldGrabbedWindowRect = this._grabbedWindowsAboutToUntileMap.get(grabbedWindow);
+        const currentRect = grabbedWindow.get_frame_rect();
+        // Untile if any of x, y, width and height changed
+        if (oldGrabbedWindowRect && 
+            (oldGrabbedWindowRect.x !== currentRect.x 
+            || oldGrabbedWindowRect.y !== currentRect.y
+            || oldGrabbedWindowRect.width !== currentRect.width
+            || oldGrabbedWindowRect.height !== currentRect.height)) 
+        {
+            const window_tiling = windowsAboutToTileMap.get(grabbedWindow);
+            const anotherTilingWindow = this._getWindowAboutToResize(window_tiling);
+
+            this._log.debug(`Untiling ${grabbedWindow.get_title()}`);
+            windowsAboutToTileMap.delete(grabbedWindow);
+
+            if (anotherTilingWindow) {
+                this._log.debug(`Untiling ${anotherTilingWindow.get_title()}`);
+                windowsAboutToTileMap.delete(anotherTilingWindow);
+            }
+
+            this._grabbedWindowsAboutToUntileMap.delete(grabbedWindow);
+        }
 
         if (this._sizeChangedId) {
             grabbedWindow.disconnect(this._sizeChangedId);
@@ -103,10 +114,40 @@ class Resizing {
 
     }
 
+    _getWindowAboutToResize(window_tiling) {
+        if (!window_tiling) return null; 
+        const window_tile_for = window_tiling.window_tile_for;
+        const shellApp = this._defaultAppSystem.lookup_app(window_tile_for.desktop_file_id);
+        if (!shellApp) return null;
+        const windows = shellApp.get_windows();
+        if (!windows || !windows.length) return null;
+
+        let windowAboutToResize = null;
+        if (windows.length === 1) {
+            windowAboutToResize = windows[0];
+        } else {
+            // Match title
+            for (const win of windows) {
+                if (win.get_title() === window_tile_for.window_title) 
+                {
+                    windowAboutToResize = win;
+                    break;
+                }
+            }
+        }
+
+        return windowAboutToResize;
+    }
+
     destroy() {
         if (windowsAboutToTileMap) {
             windowsAboutToTileMap.clear();
             windowsAboutToTileMap = null;
+        }
+
+        if (this._grabbedWindowsAboutToUntileMap) {
+            this._grabbedWindowsAboutToUntileMap.clear();
+            this._grabbedWindowsAboutToUntileMap = null;
         }
 
         if (this._grabOpBeginId) {
